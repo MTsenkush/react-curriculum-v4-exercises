498844dd2d9a751ebbe78e2b6849f17de5d36c8f
Mariia Tsenkush <239046230+MTsenkush@users.noreply.github.com>
2026-07-07 22:22:54 -0500

    lesson-01

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,29 @@
 
 export default function StudentWork() {
   //add variables here
+  const name = 'Mariia';
+  const age = 39;
+  const hobbies = [
+    'Stydying',
+    'Dog training',
+    'Reading',
+    'Traveling',
+    'Moovies',
+    'Gym',
+  ];
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About me.</h1>
+      <p>
+        My name is {name} and I am {age} years old.
+      </p>
+      <h2>My hobbies:</h2>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
